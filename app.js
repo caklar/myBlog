37ce407c62a95f9838331c88e22f819c1b68137b
caklar <acuihz123456@gmail.com>
2020-09-09 11:07:13 +0800

    连接数据库

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
-var express = require('express')
-var router = require('./router')
+const express = require('express')
+const router = require('./router')
+const mysql = require('mysql')
 
-var app = express()
+const app = express()
 
 app.use(router)
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '123698745'    
+})
+
+db.connect(function(err) {
+    if (err) throw err;
+    console.log('连接成功')
+})
 
 app.listen(3000, function (){
     console.log('running......')
