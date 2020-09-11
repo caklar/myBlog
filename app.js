e533283518105805c22fe074edb9f1cefa55428d
caklar <acuihz123456@gmail.com>
2020-09-11 09:41:13 +0800

    入口模块

--- app.js
+++ app.js
@@ -1,22 +1,14 @@
 const express = require('express')
 const router = require('./router')
-const mysql = require('mysql')
+const bodyParser = require('body-parser')
 
 const app = express()
 
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use(bodyParser.json())
+
 app.use(router)
-
-const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '123698745',
-    database: 'blog'    
-})
-
-db.connect(function(err) {
-    if (err) throw err;
-    console.log('连接成功')
-})
 
 app.listen(3000, function (){
     console.log('running......')
