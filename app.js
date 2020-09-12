c80e7299e80cdc4709cdbd32901ee9f76770d03e
caklar <acuihz123456@gmail.com>
2020-09-12 17:04:19 +0800

    项目入口

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const express = require('express')
+const path = require('path')
 const router = require('./router')
 const bodyParser = require('body-parser')
 
 const app = express()
 
+// 处理 post 请求
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(bodyParser.json())
+
+// 使用 art-template 模板引擎
+app.engine('html', require('express-art-template'));
+app.set('view options', {
+    debug: process.env.NODE_ENV !== 'production'
+});
 
 app.use(router)
 
