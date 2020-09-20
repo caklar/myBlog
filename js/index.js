f6d997e189be89f4672332095d1fd213756c80fe
caklar <acuihz123456@gmail.com>
2020-09-20 22:27:11 +0800

    首页

--- js/index.js
+++ js/index.js
@@ -1,16 +1,40 @@
 $(function () {
     // 设置分页的路径
-    $('.paging-inner a').each(function () {
-        pageRoute = '/article/page/' + $(this).text()
+    $('.paging-inner .page').each(function () {
+        if ($(this).text() == 1){
+            pageRoute = '/'
+        } else {
+            pageRoute = '/article/page/' + $(this).text()
+        }
         $(this).attr('href', pageRoute)
     })
-    // 首页
-    $('#first').attr('href', '/page/1')
 
-    // 页面切换操作
-    $('.paging-inner').on('click', 'a', function () {
-        console.log('1')
-        $(this).parent().siblings().children('a').removeClass('currentPage')
-        $(this).addClass('currentPage')
-    })
+    // 获取当前页数
+    // let pageCount = $('.page').length
+    let pageCount = $('#next').parent().prev().children('.page').text()
+    let currentPage = $('.currentPage').text()
+    let prevPage =  currentPage - 1
+    let nextPage = parseInt(currentPage) + 1
+    // 首页
+    $('#first').attr('href', '/')
+    // 上一页
+    if (currentPage == 1) {
+        // 当前页为首页时禁用上一页
+        $('#prev').addClass('currentPage')
+    } else {
+        if (prevPage == 1) {
+            $('#prev').attr('href', '/')
+        } else {
+            $('#prev').attr('href', '/article/page/' + prevPage)
+        }
+    }
+    // 下一页
+    if (currentPage == pageCount) {
+        // 当前页为末页时禁用下一页
+        $('#next').addClass('currentPage')
+    } else {
+        $('#next').attr('href', '/article/page/' + nextPage)
+    }
+    // 末页
+    $('#last').attr('href', '/article/page/' + pageCount)
 })
